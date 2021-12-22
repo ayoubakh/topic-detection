09073dfd249ad22da972f5b5053e54ef43450222
AKH-cpu <ayoubakhadam02@gmail.com>
2021-12-22 21:24:26 +0100

    change navbar color and font

--- navbar.py
+++ navbar.py
@@ -7,6 +7,13 @@ def navbar():
         {'id':'Topic Tracking', 'icon': 'fas fa-angle-double-right', 'label':'Topic Tracking'},
     ] 
 
-    over_them = {'txc_inacive': '#FFFFF'}
-    menu_id = hc.nav_bar(menu_definition=menu_data, override_theme=over_them,home_name='Home', first_select=0)
+    over_them = {'txc_inacive': '#FFFFFF'}
+    menu_id = hc.nav_bar(menu_definition=menu_data, 
+                        override_theme=over_them,
+                        home_name='Home', 
+                        first_select=0,
+                        hide_streamlit_markers=False, #will show the st hamburger as well as the navbar now!
+                        sticky_nav=True, #at the top or not
+                        sticky_mode='pinned', #jumpy or not-jumpy, but sticky or pinned
+                        )
     return menu_id
