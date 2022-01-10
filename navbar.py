b837def3ffd990e6b373a00c62bb4814c05ac5c5
AKH-cpu <ayoubakhadam02@gmail.com>
2022-01-10 11:13:03 +0100

    delete topic tracking menu

--- navbar.py
+++ navbar.py
@@ -1,10 +1,11 @@
 import hydralit_components as hc
+
 def navbar():
     menu_data = [
         {'id':'Scrape Data From Twitter', 'icon': 'fab fa-twitter', 'label':'Scrape Data From Twitter'},
         {'id':'Text Preprocessing', 'icon': 'fas fa-cogs', 'label':'Text Preprocessing'},
         {'id':'Topic Detection', 'icon': 'fas fa-search', 'label':'Topic Detection'},
-        {'id':'Topic Tracking', 'icon': 'fas fa-angle-double-right', 'label':'Topic Tracking'},
+      #  {'id':'Topic Tracking', 'icon': 'fas fa-angle-double-right', 'label':'Topic Tracking'},
     ] 
 
     over_them = {'txc_inacive': '#FFFFFF'}
@@ -14,6 +15,8 @@ def navbar():
                         first_select=0,
                         hide_streamlit_markers=False, #will show the st hamburger as well as the navbar now!
                         sticky_nav=True, #at the top or not
-                        sticky_mode='pinned', #jumpy or not-jumpy, but sticky or pinned
-                        )
+                        sticky_mode='pinned') #jumpy or not-jumpy, but sticky or pinned
+        
+                        
+                        
     return menu_id
