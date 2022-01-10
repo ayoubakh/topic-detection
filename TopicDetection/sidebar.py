15fc057435c42b2213bf4744378e984b07a49cb4
AKH-cpu <ayoubakhadam02@gmail.com>
2022-01-10 11:10:05 +0100

    add sidebar

--- TopicDetection/sidebar.py
+++ TopicDetection/sidebar.py
@@ -5,5 +5,6 @@ def sidebar():
                 used to classify text in a document to a particular topic."
 
 
+
     st.sidebar.header('Latent Dirichlet Allocation')
     st.sidebar.caption(lda_desc)
