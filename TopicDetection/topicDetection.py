ae099086a09f0a429c59de2e71b850c8b6cf4cd9
AKH-cpu <ayoubakhadam02@gmail.com>
2022-01-26 12:22:44 +0100

    add train lda model button

--- TopicDetection/topicDetection.py
+++ TopicDetection/topicDetection.py
@@ -17,24 +17,28 @@ def topicDetection():
 
         # Train the LDA model
         st.subheader('Train LDA Model')
-        parametres = get_lda_params()
-        with st.spinner("Training LDA model in progress ... "):
-                id2word, corpus, lda_model = train_lda(tweets,parametres)
+        with st.form('lda'):
+            parametres = get_lda_params()
+            submitted = st.form_submit_button('Train')
+        
+        if submitted:
+            with st.spinner("Training LDA model in progress ... "):
+                    id2word, corpus, lda_model = train_lda(tweets,parametres)
         
 
-        # LDA Results
-        topics = lda_model.show_topics(formatted=False, num_words=50,num_topics=parametres['num_topics'], log=False)
-        get_lda_results(topics)
-        
-        
-        # LDA Evaluation
-        with st.spinner('Calculating coherence metric'):
-            data = tweets.clean_tweet.values.tolist()
-            data_words = list(sent_to_words(data))
-            coherence = calculate_coherence(lda_model, data_words, id2word, 'c_v')
-            with st.expander('LDA Evaluation metric'):
-                st.write('Coherence')
-                st.write(coherence)
+            # LDA Results
+            topics = lda_model.show_topics(formatted=False, num_words=50,num_topics=parametres['num_topics'], log=False)
+            get_lda_results(topics)
+            
+            
+            # LDA Evaluation
+            with st.spinner('Calculating coherence metric'):
+                data = tweets.clean_tweet.values.tolist()
+                data_words = list(sent_to_words(data))
+                coherence = calculate_coherence(lda_model, data_words, id2word, 'c_v')
+                with st.expander('LDA Evaluation metric'):
+                    st.write('Coherence')
+                    st.write(coherence)
 
 
         #st.subheader('Generate Pyldavis')
