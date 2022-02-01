a4e283da6479734a26b7da354be6ea28d69baa83
AKH-cpu <ayoubakhadam02@gmail.com>
2022-02-01 11:55:34 +0100

    uncomment ntl download corpus

--- textPreprocessing.py
+++ textPreprocessing.py
@@ -7,7 +7,7 @@ nltk.download('averaged_perceptron_tagger')
 nltk.download('wordnet')
 nltk.download('punkt')
 nltk.download('stopwords')
-nltk.download('corpus')
+#nltk.download('corpus')
 from nltk.corpus import wordnet
 from nltk import WordNetLemmatizer
 from nltk.corpus import stopwords
